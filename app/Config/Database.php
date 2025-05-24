5ccb1d8675113b29c4f6fb73fcaeb2db721578bf
jefersonlg <jefersonlg@gmail.com>
2025-05-24 17:09:33 -0300

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -28,7 +28,7 @@ class Database extends Config
         'DSN'          => '',
         'hostname'     => 'localhost',
         'username'     => '',
-        'password'     => '3',
+        'password'     => '',
         'database'     => '',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
